81c1e8714aa2e3e57c0cc0825f9ac3a1b54062f0
wumo <wumo@outlook.com>
2020-06-29 10:51:59 +0800

    c/c++ compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,56 @@
 plugins {
-  base
-  `java-library`
-  `kotlin-dsl`
-  `maven-publish`
-  id("com.gradle.plugin-publish") version "0.11.0"
+    base
+    `java-library`
+    `kotlin-dsl`
+    `maven-publish`
+    id("com.gradle.plugin-publish") version "0.11.0"
 }
 
 group = "com.github.wumo"
-version = "1.0.9"
+version = "1.0.10"
 
 pluginBundle {
-  website = "https://github.com/wumo/gradle-javacpp-plugin"
-  vcsUrl = "https://github.com/wumo/gradle-javacpp-plugin.git"
-  tags = listOf("javacpp", "kotlin")
+    website = "https://github.com/wumo/gradle-javacpp-plugin"
+    vcsUrl = "https://github.com/wumo/gradle-javacpp-plugin.git"
+    tags = listOf("javacpp", "kotlin")
 }
 
 gradlePlugin {
-  plugins {
-    register("JavaCPPPlugin") {
-      id = "com.github.wumo.javacpp"
-      displayName = "javacpp"
-      description = "simplified javacpp plugin"
-      implementationClass = "JavaCPPPlugin"
+    plugins {
+        register("JavaCPPPlugin") {
+            id = "com.github.wumo.javacpp"
+            displayName = "javacpp"
+            description = "simplified javacpp plugin"
+            implementationClass = "JavaCPPPlugin"
+        }
     }
-  }
 }
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-  implementation("net.lingala.zip4j:zip4j:2.6.0")
+    implementation("net.lingala.zip4j:zip4j:2.6.0")
 }
 
 tasks {
-  compileKotlin {
-    kotlinOptions.jvmTarget = "1.8"
-  }
-  compileTestKotlin {
-    kotlinOptions.jvmTarget = "1.8"
-  }
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
 
 val kotlinSourcesJar by tasks
 
 publishing {
-  publications {
-    create<MavenPublication>("maven") {
-      from(components["kotlin"])
-      artifact(kotlinSourcesJar)
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+            artifact(kotlinSourcesJar)
+        }
     }
-  }
 }
 
