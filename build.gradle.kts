bac02c822ad4d44085e5c32a56133d75b1bfcf45
wumo <wumo@outlook.com>
2020-07-14 10:12:42 +0800

    [release]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,76 +1,73 @@
 plugins {
-    base
-    `java-library`
-    `kotlin-dsl`
-    `maven-publish`
-    id("com.gradle.plugin-publish") version "0.11.0"
+  base
+  `java-library`
+  `kotlin-dsl`
+  `maven-publish`
+  id("com.gradle.plugin-publish") version "0.11.0"
 }
 
 group = "com.github.wumo"
 version = "1.0.11"
 
 pluginBundle {
-    website = "https://github.com/wumo/gradle-javacpp-plugin"
-    vcsUrl = "https://github.com/wumo/gradle-javacpp-plugin.git"
-    tags = listOf("javacpp", "kotlin")
+  website = "https://github.com/wumo/gradle-javacpp-plugin"
+  vcsUrl = "https://github.com/wumo/gradle-javacpp-plugin.git"
+  tags = listOf("javacpp", "kotlin")
 }
 
 gradlePlugin {
-    plugins {
-        register("JavaCPPPlugin") {
-            id = "com.github.wumo.javacpp"
-            displayName = "javacpp"
-            description = "simplified javacpp plugin"
-            implementationClass = "JavaCPPPlugin"
-        }
+  plugins {
+    register("JavaCPPPlugin") {
+      id = "com.github.wumo.javacpp"
+      displayName = "javacpp"
+      description = "simplified javacpp plugin"
+      implementationClass = "JavaCPPPlugin"
     }
+  }
 }
 
-val setupPublishEnv by tasks.creating {
-    doLast {
-        val key = System.getenv("GRADLE_PUBLISH_KEY")
-        val secret = System.getenv("GRADLE_PUBLISH_SECRET")
-
-        if (key == null || secret == null) {
-            throw GradleException("gradlePublishKey and/or gradlePublishSecret are not defined environment variables")
-        }
-
-        System.setProperty("gradle.publish.key", key)
-        System.setProperty("gradle.publish.secret", secret)
-    }
-}
-
-val publishPluginFromEnv by tasks.creating {
-    dependsOn(setupPublishEnv)
-    dependsOn(tasks.publishPlugins.get())
-    tasks.publishPlugins.get().mustRunAfter(setupPublishEnv)
-}
-
+//val publishPluginFromEnv by tasks.creating {
+//  val setupPublishEnv by tasks.creating {
+//    doLast {
+//      val key = System.getenv("GRADLE_PUBLISH_KEY") ?: error("GRADLE_PUBLISH_KEY not set")
+//      val secret = System.getenv("GRADLE_PUBLISH_SECRET") ?: error("GRADLE_PUBLISH_SECRET not set")
+//
+//      System.setProperty("gradle.publish.key", key)
+//      System.setProperty("gradle.publish.secret", secret)
+//    }
+//  }
+//  tasks.login{
+//    this.login()
+//  }
+//  dependsOn(setupPublishEnv)
+//  dependsOn(tasks.publishPlugins.get())
+//  tasks.publishPlugins.get().mustRunAfter(setupPublishEnv)
+//}
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("net.lingala.zip4j:zip4j:2.6.0")
+  implementation("net.lingala.zip4j:zip4j:2.6.0")
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
+  compileKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+  compileTestKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+  }
 }
 
 val kotlinSourcesJar by tasks
 
 publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["kotlin"])
-            artifact(kotlinSourcesJar)
-        }
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["kotlin"])
+      artifact(kotlinSourcesJar)
     }
+  }
 }
 
